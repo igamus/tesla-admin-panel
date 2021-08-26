85fe855de9b503fa59b43849bd5d127aff47a18a
Isaac Gamus <igamus@gmail.com>
2021-08-26 02:10:48 -0400

    Added navigation to home when logo is clicked

--- resources/js/components/client/ClientNavigator.vue
+++ resources/js/components/client/ClientNavigator.vue
@@ -1,11 +1,13 @@
 <template>
     <span>
-        <v-toolbar app color="black" dark>
+        <v-toolbar color="black" dark>
                 <v-toolbar-title>
+                    <router-link to ="/home">
                         <v-img src="/assets/tesla-lo.png" height="50px" width="50px"></v-img>
+                    </router-link>
                 </v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-btn flat class="hidden-sm-and-down white font-weight-bold black--text">Specifications</v-btn>
+                <v-btn class="hidden-sm-and-down white font-weight-bold black--text">Specifications</v-btn>
                 <v-btn class="hidden-sm-and-down white font-weight-bold black--text ml-3">Order Now</v-btn>
         </v-toolbar>
     </span>
